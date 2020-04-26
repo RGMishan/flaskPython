bc4e45faf908d409bc3ea08ac4e3538eb736f01d
RGMishan <mishanregmi@gmail.com>
2020-04-26 19:04:46 +0530

    variables

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+
+print('Type your name>>')
+name = input()
+print(f"hello, {name}!") #F is to format the string
+
+''' This is also a comment '''
---
+++ variable.py
@@ -0,0 +1,11 @@
+i = 28
+print(f"i is {i}")
+
+f = 2.8
+print(f"i is {f}")
+
+b = True
+print(f"i is {b}")
+
+n = None
+print(f"n is {n}")
