4f2a48391d84c44ca0da42fbd65261579fae07f2
RGMishan <mishanregmi@gmail.com>
2020-04-26 23:29:17 +0530

    loops in py

--- loop.py
+++ loop.py
@@ -1,2 +1,16 @@
 for i in range(5):
     print(i)
+
+names= ["Alice", "Mishan", "Regmi"]
+print( names[2] )
+print()
+
+for name in names:
+    print(name)
+
+print()
+print("List with different datatype")
+lists= ["Alice", 28, 5*5] #Its good to have same type element but we can use different
+print()
+for listt in lists:
+    print(listt)
