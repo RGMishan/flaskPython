211f892d4603bc4d098ab914d7b65c3e0469a251
RGMishan <mishanregmi@gmail.com>
2020-04-26 22:36:58 +0530

    condition in py

---
+++ condition.py
@@ -0,0 +1,10 @@
+print(f"Enter a number.")
+x = int (input())
+
+#indention in python is most important
+if x > 0:
+    print("x is positive number")
+elif x < 0:
+    print("x is negetive number")
+else:
+    print("x is equal to zero")
